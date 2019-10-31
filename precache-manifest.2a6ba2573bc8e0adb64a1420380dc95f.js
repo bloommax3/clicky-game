cbbe973dc8bf8a7c1880eaec74389204657d0233
MAX_BLOOM <bloommax3@gmail.com>
2019-10-30 23:39:05 -0400

    Updates

---
+++ precache-manifest.2a6ba2573bc8e0adb64a1420380dc95f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bca0f89c6e696ef6f8b7e00ce2b8c80",
+    "url": "/bloommax3/clicky-game.git/index.html"
+  },
+  {
+    "revision": "ed6b4ae6cbcfb9e5342d",
+    "url": "/bloommax3/clicky-game.git/static/css/main.3c6c1d55.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/bloommax3/clicky-game.git/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "ed6b4ae6cbcfb9e5342d",
+    "url": "/bloommax3/clicky-game.git/static/js/main.8c645e3a.chunk.js"
+  },
+  {
+    "revision": "3112709bc21e27a32209",
+    "url": "/bloommax3/clicky-game.git/static/js/runtime-main.07f6312c.js"
+  }
+]);
